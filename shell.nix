69a36e12d1674d18eb0cf765ad5e633586ac0153
Andras Sore <andras08@gmail.com>
2022-09-24 11:15:41 +0200

    Move default.nix to shell.nix. Add zls.

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ sources ? import ./nix/sources.nix
+, pkgs ? import sources.nixpkgs {}
+}:
+
+let
+  zig = pkgs.stdenv.mkDerivation {
+    name = "zig";
+    src = sources.zig;
+    installPhase = ''
+      mkdir -p $out/bin
+      mv zig $out/bin
+      mv lib $out
+    '';
+    dontStrip = true;
+  };
+in pkgs.mkShell {
+  buildInputs = [
+    zig
+    pkgs.zls
+    pkgs.niv
+  ];
+}
