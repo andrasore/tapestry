8d454cc1674fa1ad518e930f053c598cef02c78d
Andras Sore <andras08@gmail.com>
2022-10-04 20:25:41 +0200

    start implementing isometric tiles

--- src/constants.zig
+++ src/constants.zig
@@ -1,16 +1,20 @@
 
 pub const img_height = 1080;
 pub const img_width = 1920;
-pub const tile_size = 40;
-pub const img_rows = img_height / tile_size;
-pub const img_columns = img_width / tile_size;
+pub const tile_height = 15;
+pub const tile_width = 30;
+pub const img_rows = img_height / tile_height;
+pub const img_columns = img_width / tile_width;
 pub const pixel_count = img_height * img_width;
 
 comptime {
+    if (tile_height % 2 != 1) {
+        @compileError("tile_height should be an odd number!");
+    }
     if (img_rows % 1 != 0) {
-        @compileError("'img_height / tile_size' should be a whole number!");
+        @compileError("'img_height / tile_height' should be a whole number!");
     }
     if (img_columns % 1 != 0) {
-        @compileError("'img_width / tile_size' should be a whole number!");
+        @compileError("'img_width / tile_width' should be a whole number!");
     }
 }
--- src/generator.zig
+++ src/generator.zig
@@ -16,18 +16,30 @@ var pixel_presets = [4]Pixel{
     Pixel{ .r = 0, .g = 100, .b = 150 },
 };
 
-fn fillSquare(img: *Image, offset_x: usize, offset_y: usize, rand: Random) void {
-    const choice = rand.intRangeLessThan(usize, 0, pixel_presets.len);
+pub const Image = [constants.img_height][constants.img_width]Pixel;
+pub const Tile = [constants.tile_height][constants.tile_width]Pixel;
+
+fn fillTile(img: *Image, offset_x: usize, offset_y: usize, tile: *Tile) void {
     var x: usize = 0;
     while (x < constants.tile_size) : (x += 1) {
         var y: usize = 0;
         while (y < constants.tile_size) : (y += 1) {
-            img[x + offset_x][y + offset_y] = pixel_presets[choice];
+            img[x + offset_x][y + offset_y] = tile[x][y];
         }
     }
 }
 
-pub const Image = [constants.img_height][constants.img_width]Pixel;
+fn generateTile(alloc: Allocator) !*Tile {
+    const tile = try alloc.create(Tile);
+    var x: usize = 0;
+    while (x < constants.tile_size) : (x += 1) {
+        var y: usize = 0;
+        while (y < constants.tile_size) : (y += 1) {
+            tile[x][y] = Pixel{ .r = 100, .g = 20, .b = 130 };
+        }
+    }
+    return tile;
+}
 
 pub fn generateImg(alloc: Allocator) !*Image {
     var img = try alloc.create(Image);
@@ -37,7 +49,7 @@ pub fn generateImg(alloc: Allocator) !*Image {
         try std.os.getrandom(std.mem.asBytes(&seed));
         break :blk seed;
     });
-    var rand = prng.random();
+    _ = prng.random();
 
     var row: usize = 0;
     while (row < constants.img_rows) : (row += 1) {
@@ -45,7 +57,9 @@ pub fn generateImg(alloc: Allocator) !*Image {
         while (column < constants.img_columns) : (column += 1) {
             const offset_x = row * constants.tile_size;
             const offset_y = column * constants.tile_size;
-            fillSquare(img, offset_x, offset_y, rand);
+            const tile = try generateTile(alloc);
+            defer alloc.free(tile);
+            fillTile(img, offset_x, offset_y, tile);
         }
     }
 
